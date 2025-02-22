85cd75ba2c2654cc37968790d7865d1fd732958b
Devashish Chakraborty <devashishchakra@gmail.com>
2025-02-23 01:53:49 +0530

    Added custom port support

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import authRouter from "./routes/authRouter.js";
 import vaultRouter from "./routes/vaultRouter.js";
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.set("views", path.join(import.meta.dirname, "views"));
 app.set("view engine", "ejs");
@@ -33,4 +34,4 @@ app.use((err, req, res, next) => {
   res.status(err.statusCode || 500).render("404", { message: err.message });
 });
 
-app.listen(3000, () => console.log("app listening on port 3000!"));
+app.listen(port, () => console.log(`app listening on port ${port}!`));
